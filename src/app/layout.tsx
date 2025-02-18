03e0735b846ca145eaa4d8091f13b8710be20c25
s1099 <46890315+s1099@users.noreply.github.com>
2025-02-18 20:04:01 +0530

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { ThemeProvider } from "./providers/theme-provider";
-
+import { Analytics } from "@vercel/analytics/react"
 export const metadata = {
   title: "Wikipedia",
   description: "A Wikipedia frontend",
@@ -13,6 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+      {process.env.VERCEL_ANALYTICS && <Analytics/>}
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
